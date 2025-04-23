8f48cbce505b1325706c48503fb9949f55dfc8a8
Jhon Lynikeer <jhonlynikeer@gmail.com>
2025-04-23 15:35:30 -0300

    commit-initial

---
+++ src/main/kotlin/com/coopdev/Push/Automatic/PushAutomaticApplication.kt
@@ -0,0 +1,11 @@
+package com.coopdev.Push.Automatic
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PushAutomaticApplication
+
+fun main(args: Array<String>) {
+	runApplication<PushAutomaticApplication>(*args)
+}
---
+++ src/test/kotlin/com/coopdev/Push/Automatic/PushAutomaticApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.coopdev.Push.Automatic
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PushAutomaticApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
